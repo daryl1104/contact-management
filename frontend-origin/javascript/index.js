499a11e9c8433882dd40f1ede6ef1ff4ed8aab8a
jasmine.deng <jasmine.deng@yuyutechnology.com>
2023-07-30 21:33:37 +0800

    small fix

--- frontend-origin/javascript/index.js
+++ frontend-origin/javascript/index.js
@@ -3,5 +3,36 @@ function handleAddButton(e) {
     e.preventDefault();
     const ele = document.getElementById("add-form-p");
     ele.style.display = "block";
+    const detailEle = document.getElementById("contact-detail");
+    detailEle.style.display = "none";
 }
 
+function handleEditButton(e) {
+    e.preventDefault();
+    const detailEle = document.getElementById("contact-detail");
+    detailEle.style.display = "none";
+    const ele = document.getElementById("add-form-p");
+    ele.style.display = "block";
+
+    var contactData = {};
+    contactData.name = "啊";
+    contactData.phone = "13111110000";
+    contactData.gender = "男";
+    console.log(contactData);
+    localStorage.setItem("contact", JSON.stringify(contactData));
+
+    // 数据回填
+    // 从localStorage里面拿数据。（数据是每次点击左边，除了去请求http后端拿详细数据，set到localStorage里面）
+    const currentContact = localStorage.getItem("contact");
+    const currentContactObjet = JSON.parse(currentContact);
+
+    const name = currentContactObjet.name;
+    const phone = currentContactObjet.phone;
+
+    const hone = ele.getElementsByTagName("h1");
+    hone[0].innerHTML = "修改联系人";
+
+
+
+
+}
